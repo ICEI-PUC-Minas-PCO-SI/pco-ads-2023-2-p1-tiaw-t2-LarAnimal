92224332b7a52d2f6ad8481b193955033707f5f4
rhillary05 <rhillaryp.carvalho@gmail.com>
2023-12-05 21:41:46 -0300

    correção de conflitos

--- src/main_forum.js
+++ src/main_forum.js
@@ -1,13 +1,4 @@
 //URL DA API DE DADOS
-<<<<<<< HEAD
-URL = 'http://localhost:3000'
-
-//============================================================================================================
-//GET - Recuperação de dados e adição dos posts default
-
-const postsList = document.getElementById('posts');
-
-=======
 URL = 'http://localhost:3000/posts'
 let ULL = "http://localhost:3000/comentarios"
 //============================================================================================================
@@ -119,67 +110,12 @@ function novodado() {
 }
 //=================================================================================
 //GET - recupera os dados 
->>>>>>> f_forum
 fetch(URL)
     .then(res => res.json())
     .then(threads => {
         let posts_respostas = '';
         for (let i = 0; i < threads.length; i++) {
             posts_respostas += `
-<<<<<<< HEAD
-            <div class="thread">
-               <h3>${threads[i].post - title}</h3>
-                 <div class="respostas">
-                      <p>${posts[i].comment - content}</p>
-                        <hr>
-                 </div>
-             </div>
-             `;
-             postsList.innerHTML = posts_respostas;
-        }
-
-    }
-    );
-
-    //===================================================================================
-    //CREATE OR UPDATE - Procedimento para criar o editar um post 
-
-    const postForm = document.getElementById('post-form');
-
-    postForm.addEventListener('submit', (e) => {
-        let id = parseInt($('#post-content').text());
-
-        const novo_post = JSON.stringify({
-            post_content: document.getElementById('post-content').value
-        })
-
-        if (post-content >=""){
-            fetch(`${URL}/${post-content}`,{
-                method: 'PUT',
-                headers: {
-                    'Content-Type' : 'aplication/json'
-                },
-                body: novo-post
-            })
-            .then(res => res.json())
-            .then(() => location.reload());
-        }
-        else{
-            fetch(URL, {
-                method: 'POST',
-                headers: {
-                    'Content-Type' : 'aplication/json'
-                },
-                body: novo-post
-            })
-            .then(res => res.json())
-            .then (() => location.reload());
-        }
-    })
-
-    //======================================================================
-    
-=======
              <div class="thread">
                 <h3>${threads[i].post - title}</h3>
                   <div class="respostas">
@@ -193,4 +129,3 @@ fetch(URL)
     }
     );
     //========================================================================
->>>>>>> f_forum
