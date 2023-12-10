4baf1c570da0393c946a950e67f22790a1088adb
jeffsantosc <jeffsantosc@gmail.com>
2023-12-10 17:40:27 -0300

    server agendamento

--- src/js/agendamento.js
+++ src/js/agendamento.js
@@ -1,6 +1,8 @@
+const baseUrl = "http://localhost:3333/agendamentos";
+
 async function loadAppointments() {
     try {
-        const response = await $.get("http://localhost:3333/agendamentos");
+        const response = await $.get(baseUrl);
         return response || [];
     } catch (error) {
         window.alert("Erro ao carregar os agendamentos");
@@ -18,6 +20,14 @@ function closePopup() {
     document.getElementById('clinicPopup').style.display = 'none';
 }
 
+function limparDadosAgendamento() {
+    document.getElementById('clinicName').textContent = ''
+    document.getElementById('clinicAddress').textContent = '';
+    document.getElementById('appointmentDate').value = '';
+    document.getElementById('appointmentTime').value = '';
+    document.getElementById('appointmentId').value = '';
+}
+
 $(document).ready(function () {
     const btnAgendamento = document.getElementById('btn-agendar');
 
@@ -27,80 +37,49 @@ $(document).ready(function () {
         const clinicName = document.getElementById('clinicName').textContent;
         const appointmentDate = document.getElementById('appointmentDate').value;
         const appointmentTime = document.getElementById('appointmentTime').value;
+        const id = document.getElementById('appointmentId').value;
 
-        const novoAgendamento = {
+        const agendamento = {
             clinicName,
             appointmentDate,
-            appointmentTime
-        };async function loadAppointments() {
-    try {
-        const response = await fetch("http://localhost:3333/agendamentos");
-        if (response.ok) {
-            const data = await response.json();
-            return data || [];
-        } else {
-            throw new Error('Erro ao carregar os agendamentos');
-        }
-    } catch (error) {
-        window.alert(error.message);
-        return [];
-    }
-}
+            appointmentTime,
+            id
+        };
 
-async function saveAppointments(appointments) {
-    try {
-        const response = await fetch("http://localhost:3333/agendamentos", {
-            method: 'PUT',
-            headers: {
-                'Content-Type': 'application/json',
+        if (id) {
+            await $.ajax({
+                url: `${baseUrl}/${agendamento.id}`,
+                type: 'PUT',
+                contentType: 'application/json',
+                data: JSON.stringify(agendamento),
+                success: async function(result) {
+                    await showAppointmentsList();
+                    limparDadosAgendamento();
+                    closePopup();
+                },
+                error: function(error) {
+                  console.error('Error in PUT request', error);
+                }
+              });
+              return;
+        }
+
+        $.ajax({
+            url: baseUrl,
+            type: 'POST',
+            contentType: 'application/json',
+            data: JSON.stringify(agendamento),
+            success: async function(result) {
+                await showAppointmentsList();
+                limparDadosAgendamento();
+                closePopup();
+              console.log('POST request successful', result);
             },
-            body: JSON.stringify(appointments),
-        });
-        if (response.ok) {
-            const data = await response.json();
-            console.log('Agendamentos salvos:', data);
-        } else {
-            throw new Error('Erro ao salvar os agendamentos');
-        }
-    } catch (error) {
-        window.alert(error.message);
-    }
-}
-
-async function deleteAppointment(index) {
-    let existingAppointments = await loadAppointments();
-    existingAppointments.splice(index, 1);
-    await saveAppointments(existingAppointments);
-    await showAppointmentsList();
-}
-
-async function editAppointment(index) {
-    let existingAppointments = await loadAppointments();
-    const appointment = existingAppointments[index];
-
-    document.getElementById('clinicName').textContent = appointment.clinicName;
-    document.getElementById('clinicAddress').textContent = '';
-    document.getElementById('appointmentDate').value = appointment.appointmentDate;
-    document.getElementById('appointmentTime').value = appointment.appointmentTime;
-
-    // Exibir o popup
-    document.getElementById('clinicPopup').style.display = 'block';
-}
-
-// Restante do seu código...
-
-        // Restante do seu código...
-        
-
-        try {
-            const response = await $.post({
-                url: "http://localhost:3333/agendamentos",
-                data: novoAgendamento
-            });
-            console.log("POST request successful:", response);
-        } catch (error) {
-            window.alert("Erro ao cadastrar usuário");
-        }
+            error: function(error) {
+              console.error('Error in POST request', error);
+            }
+          });
+    
     });
 });
 
@@ -136,47 +115,62 @@ async function showAppointmentsList() {
     console.log('Agendamentos existentes:', existingAppointments)
     existingAppointments.forEach((appointment, index) => {
         const listItem = document.createElement('li');
-        listItem.innerHTML = `<div class="appointment-box"><strong>Clinica:</strong> ${appointment.clinicName} - <strong>Data:</strong> ${formatarDataBrasil(appointment.appointmentDate)} - <strong>Hora:</strong> ${appointment.appointmentTime}
-        <div class="appointment-buttons"><button onclick="editAppointment(${index})">Editar</button><button id="btn-delete" onclick="deleteAppointment(${index})">Excluir</button></div></div>`;
+        listItem.innerHTML = `
+        <div class="appointment-box">
+            <strong>Clinica:</strong> ${appointment.clinicName} - <strong>Data:</strong> ${formatarDataBrasil(appointment.appointmentDate)} - <strong>Hora:</strong> ${appointment.appointmentTime}
+            <div class="appointment-buttons">
+                <button id="${appointment.id}" onclick="editAppointment(${appointment.id})">Editar</button>
+                <button id="btn-delete" id="${appointment.id}" onclick=deleteAppointment(${appointment.id})>Excluir</button>
+            </div>
+        </div>`;
+
         appointmentsList.appendChild(listItem);
     });
 }
-async function deleteAppointment(index) {
-    let existingAppointments = await loadAppointments();
-    existingAppointments.splice(index, 1);
-     saveAppointments(existingAppointments);
-    await showAppointmentsList();
+
+async function editAppointment(id) {
+    const appointment = await findAppointmentById(id);
+
+    if (appointment) {
+        document.getElementById('clinicName').textContent = appointment.clinicName;
+        document.getElementById('clinicAddress').textContent = '';
+        document.getElementById('appointmentDate').value = appointment.appointmentDate;
+        document.getElementById('appointmentTime').value = appointment.appointmentTime;
+        document.getElementById('appointmentId').value = appointment.id;
+    
+        // Exibir o popup
+        document.getElementById('clinicPopup').style.display = 'block';
+    } 
 }
 
-async function editAppointment(index) {
-    let existingAppointments = await loadAppointments();
-    const appointment = existingAppointments[index];
-
-    document.getElementById('clinicName').textContent = appointment.clinicName;
-    document.getElementById('clinicAddress').textContent = '';
-    document.getElementById('appointmentDate').value = appointment.appointmentDate;
-    document.getElementById('appointmentTime').value = appointment.appointmentTime;
-
-    // Exibir o popup
-    document.getElementById('clinicPopup').style.display = 'block';
-}
-
-async function showAppointmentsList() {
-    const appointmentsList = document.getElementById('appointmentsList');
-    appointmentsList.innerHTML = '';
-
-    const existingAppointments = await loadAppointments();
-
-    console.log('Agendamentos existentes:', existingAppointments)
-    existingAppointments.forEach((appointment, index) => {
-        const listItem = document.createElement('li');
-        listItem.innerHTML = `<div class="appointment-box"><strong>Clinica:</strong> ${appointment.clinicName} - <strong>Data:</strong> ${formatarDataBrasil(appointment.appointmentDate)} - <strong>Hora:</strong> ${appointment.appointmentTime}
-        <div class="appointment-buttons"><button onclick="editAppointment(${index})">Editar</button><button id="btn-delete" onclick="deleteAppointment(${index})">Excluir</button></div></div>`;
-        appointmentsList.appendChild(listItem);
+async function findAppointmentById(id) {
+    let appointment = null;
+    await $.get(`${baseUrl}/${id}`, function (data) {
+        appointment = data;
+    }).fail(function (error) {
+        console.log("Agendamento não encontrado")
     });
+    return appointment;
+}
+
+async function deleteAppointment(id) {
+    const deleteAppointment = window.confirm("Tem certeza que deseja excluir esse atendimento? ");
+
+    if (deleteAppointment) {
+        $.ajax({
+            url: `${baseUrl}/${id}`,
+            type: 'DELETE',
+            success: async function (result) {
+                console.log('DELETE request successful', result);
+                await showAppointmentsList();
+            },
+            error: function (error) {
+                console.error('Error in DELETE request', error);
+            }
+        });
+    }
 }
 
 document.addEventListener('DOMContentLoaded', async function () {
-
     await showAppointmentsList();
 });
