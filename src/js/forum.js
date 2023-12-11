ade72a24df216405efb7c6394ea27252153f671a
jeffsantosc <jeffsantosc@gmail.com>
2023-12-11 20:07:22 -0300

    forum

--- src/js/forum.js
+++ src/js/forum.js
@@ -1,122 +1,131 @@
-function getRandomNumber(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
-const posts = [];
-
-function renderPosts() {
-    const postList = document.getElementById('post-list');
-    postList.innerHTML = '';
-    posts.forEach(post => {
-        const postElement = document.createElement('div');
-        postElement.classList.add('post', 'p-3', 'bg-light');
-
-        const userAvatar = post.avatar ? `<img src="${post.avatar}" alt="Avatar" class="user-avatar" onclick="showPopup('${post.avatar}')">` : '';
-        
-        postElement.innerHTML = `
-            ${userAvatar}
-            <strong>${post.username}</strong>: ${post.message}
-        `;
-        postList.appendChild(postElement);
-    });
-}
-
-function showPopup(avatarUrl) {
-    const popup = document.getElementById('user-popup');
-    const carousel = popup.querySelector('.carousel');
-    carousel.innerHTML = ''; 
-
-    
-    posts.forEach(post => {
-        if (post.avatar) {
-            const carouselItem = document.createElement('div');
-            carouselItem.classList.add('carousel-item');
-
-            const img = document.createElement('img');
-            img.src = post.avatar;
-            img.alt = 'Avatar';
-            img.classList.add('d-block', 'w-100');
-
-            carouselItem.appendChild(img);
-            carousel.appendChild(carouselItem);
-        }
-    });
-
-    popup.style.display = 'block';
-}
-
-function hidePopup() {
-    const popup = document.getElementById('user-popup');
-    popup.style.display = 'none';
-}
-
-function renderCommentsCarousel() {
-    const carouselInner = document.querySelector('#comment-carousel .carousel-inner');
-    carouselInner.innerHTML = '';
-
-    
-    posts.forEach((post, index) => {
-        const commentElement = document.createElement('div');
-        commentElement.classList.add('carousel-item', index === 0 ? 'active' : '');
-        commentElement.innerHTML = `
-            <h3>${post.username}</h3>
-            <p>${post.message}</p>
-        `;
-        carouselInner.appendChild(commentElement);
-    });
-}
-
-function renderUserSidebar() {
-    const userSidebar = document.querySelector('.user-sidebar');
-    
-}
-
-function renderMainComments() {
-    const mainCommentsList = document.getElementById('main-comments-list');
-   
-}
-
-function calculateAndDisplayRoute(destination) {
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(function (position) {
-            var userPos = {
-                lat: position.coords.latitude,
-                lng: position.coords.longitude
-            };
-
-            var request = {
-                origin: userPos,
-                destination: destination,
-                travelMode: 'DRIVING'
-            };
-
-            directionsService.route(request, function (response, status) {
-                if (status === 'OK') {
-                    directionsDisplay.setDirections(response);
-                } else {
-                    window.alert('Não foi possível calcular a rota: ' + status);
-                }
-            });
-        });
+//URL DA API DE DADOS
+URL = 'http://localhost:3333/posts'
+let ULL = "http://localhost:3333/comentarios"
+//============================================================================================================
+//POST - Criar um novo post ou comentário
+let enviarConte = document.getElementById("enviar-content")
+let comentario = document.getElementById("enviar-newComente")
+comentario.addEventListener("click", novoComent)
+enviarConte.addEventListener("click", novodado)
+function novoComent() {
+    let newcComent = document.getElementById("post-content").value;
+    let num = Math.random(40)
+    let coment = {
+        id: num,
+        content: newcComent
     }
+    fetch(ULL, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(coment)
+    })
+        .then(response => response.json())
+        .then(data => console.log(data))
+
+
 
 }
+function novodado() {
+    let comennte = document.getElementById("comment-content").value;
+    alert("Entrou função " + comennte)
+    let num = Math.random(40)
+    alert(num)
+    let newComent = {
+        id: num,
+        title: comennte,
+        comments: "Entrei pela primeira vez"
+    }
+    alert(newComent)
+    fetch(URL, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(newComent)
+    })
+        .then(response => response.json())
+        .then(data => console.log(data))
 
-document.getElementById('post-form').addEventListener('submit', function (event) {
-    event.preventDefault();
-    const username = document.getElementById('username').value;
-    const message = document.getElementById('post-content').value;
-    const avatarNumber = getRandomNumber(1, 5); // 
-    const avatar = `https://i.pravatar.cc/150?img=${avatarNumber}`; 
-    posts.push({ username, message, avatar });
-    renderPosts();
-    renderCommentsCarousel();
-    document.getElementById('username').value = '';
-    document.getElementById('post-content').value = '';
-});
+}
+//===========================================================================================================
+//DELETE - excluir um post ou comentário
+const comennteDelete = document.getElementById('btn-deletar');
+comennteDelete.addEventListener('click', (e) => {
+    let id = $('id');
+    fetch(`${ULL}/${id}`, {
+        method: 'DELETE',
+    })
+        .then(RES => resizeBy.json())
+        .then(() => location.reload());
+})
+//====================================================================================================================================
+// PUT - Editar um comentário ou post
+let editConte = document.getElementById("edit-comentario")
+let comenta = document.getElementById("enviar-editComente")
+comentario.addEventListener("click", novoComent)
+enviarConte.addEventListener("click", novodado)
+function novoComent() {
+    let newcComent = document.getElementById("post-content").value;
+    let num = Math.random(40)
+    let coment = {
+        id: num,
+        content: newcComent
+    }
+    fetch(ULL, {
+        method: "PUT",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(coment)
+    })
+        .then(response => response.json())
+        .then(data => console.log(data))
 
 
-renderPosts();
-renderCommentsCarousel();
-renderUserSidebar();
-renderMainComments();
+
+}
+function novodado() {
+    let comennte = document.getElementById("comment-content").value;
+    alert("Entrou função " + comennte)
+    let num = Math.random(40)
+    alert(num)
+    let newComent = {
+        id: num,
+        title: comennte,
+        comments: "Entrei pela primeira vez"
+    }
+    alert(newComent)
+    fetch(URL, {
+        method: "PUT",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(newComent)
+    })
+        .then(response => response.json())
+        .then(data => console.log(data))
+
+}
+//=================================================================================
+//GET - recupera os dados 
+fetch(URL)
+    .then(res => res.json())
+    .then(threads => {
+        let posts_respostas = '';
+        for (let i = 0; i < threads.length; i++) {
+            posts_respostas += `
+             <div class="thread">
+                <h3>${threads[i].post - title}</h3>
+                  <div class="respostas">
+                       <p>${posts[i].comment - content}</p>
+                         <hr>
+                  </div>
+              </div>
+              `;
+            postsList.innerHTML = posts_respostas;
+        }
+    }
+    );
+    //========================================================================
