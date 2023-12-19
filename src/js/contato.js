3c93ef25028b1994154caeae71dca0aef9b764f2
rhillary05 <142110921+rhillary05@users.noreply.github.com>
2023-12-18 22:15:41 -0300

    Update contato.js
    link json replit

--- src/js/contato.js
+++ src/js/contato.js
@@ -16,7 +16,7 @@ document.addEventListener('DOMContentLoaded', function () {
             motivo
         };
 
-        fetch('http://localhost:3333/contato', {
+        fetch('https://lar-animal--rhillarycarvalh.repl.co/contato', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
